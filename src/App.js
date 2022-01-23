6c52849bec164f767b2e1b71b4b3765dfbe6e0f6
Armanul Ambia <arman@Arman-M1.local>
2022-01-23 14:21:43 -0800

    finalized frontend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
+
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
@@ -10,10 +10,7 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-//You must add your own API key here from Clarifai.
-const app = new Clarifai.App({
- apiKey: 'fc4fdc4b637249f2acd8e1a519cfff40'
-});
+
 
 const particlesOptions = {
   particles: {
@@ -27,6 +24,21 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
@@ -34,7 +46,7 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'home',
+      route: 'signin',
       isSignedIn: false,
       user: {
         id: '',
@@ -45,7 +57,6 @@ class App extends Component {
       }
     }
   }
-
   loadUser = (data) => {
     this.setState({user: {
       id: data.id,
@@ -57,6 +68,7 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
+    
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
@@ -79,38 +91,46 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
-      .then(response => {
-        console.log('hi', response)
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              id: this.state.user.id
+    if(this.state.input.length){
+      fetch('http://localhost:3000/imageurl', {
+              method: 'post',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                input: this.state.input
+              })
+        })
+        .then(response => response.json())
+        .then(response => {
+          console.log('hi', response)
+          if (response) {
+            fetch('http://localhost:3000/image', {
+              method: 'put',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                id: this.state.user.id
+              })
             })
-          })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
-            })
+              .then(response => response.json())
+              .then(count => {
+                this.setState(Object.assign(this.state.user, { entries: count}))
+              })
+              .catch(console.log)
 
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      })
-      .catch(err => console.log(err));
+          }
+          this.displayFaceBox(this.calculateFaceLocation(response))
+        })
+        .catch(err => console.log(err));
+    }
+    
   }
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
-      this.setState({isSignedIn: true})
+      this.setState({isSignedIn: true, imageUrl:''})
     }
-    this.setState({route: route});
+    this.setState({route: route, imageUrl:''});
   }
 
   render() {
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
-  return (
-    <div>
-      <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
-      </p>
-      <div className='center'>
-        <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
-          <button
-            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
-            onClick={onButtonSubmit}
-          >Detect</button>
+
+class ImageLinkForm extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      val: ''
+    };
+  }
+  onClick = (e) => {
+    this.props.onButtonSubmit(e)
+    this.setState({val:''})
+  }
+  onChange = (e) => {
+    this.props.onInputChange(e)
+    this.setState({val: e.target.value})
+  }
+  render () {
+    return (
+      <div>
+        <p className='f3'>
+          {'This Magic Brain will detect faces in your pictures. Git it a try.'}
+        </p>
+        <div className='center'>
+          <div className='form center pa4 br3 shadow-5'>
+            <input className='f4 pa2 w-70 center' type='tex' onChange={this.onChange} value = {this.state.val}/>
+            <button
+              className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+              onClick={this.onClick}
+            >Detect</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+};
+
 
 export default ImageLinkForm;
